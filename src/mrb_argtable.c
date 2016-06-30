9d9804233d10028bba108ebeba1714ca29ed851b
Uchio KONDO <udzura@pepabo.com>
2016-06-30 16:51:02 +0900

    Add handler for arg_lit

--- src/mrb_argtable.c
+++ src/mrb_argtable.c
@@ -7,6 +7,7 @@
 */
 
 #include "mruby.h"
+#include "mruby/array.h"
 #include "mruby/data.h"
 #include "mrb_argtable.h"
 #include "argtable3.h"
@@ -16,20 +17,72 @@
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
-typedef struct {
-  char *str;
-  int len;
+typedef struct mrb_argtable_data {
+  void **argtable;
+  mrb_value arg_definitions;
 } mrb_argtable_data;
 
+void mrb_argtable_free(mrb_state *mrb, void *p)
+{
+  mrb_argtable_data *argtable_s = (mrb_argtable_data *)p;
+  arg_freetable(argtable_s->argtable, sizeof(argtable_s->argtable)/sizeof(argtable_s->argtable[0]));
+  mrb_free(mrb, argtable_s);
+}
+
+#define MRB_ARGTABLE_DATA_TYPE(argtable_s)                               \
+  typedef struct argtable_s ## _data {                                   \
+    struct argtable_s *definition;                                       \
+  } argtable_s;                                                          \
+  static const struct mrb_data_type mrb_ ## argtable_s ## _data_type = { \
+    #argtable_s, mrb_free,                                               \
+  }
+
+MRB_ARGTABLE_DATA_TYPE(arg_lit);
+MRB_ARGTABLE_DATA_TYPE(arg_int);
+MRB_ARGTABLE_DATA_TYPE(arg_dbl);
+MRB_ARGTABLE_DATA_TYPE(arg_str);
+/* MRB_ARGTABLE_DATA_TYPE(arg_rex);
+   MRB_ARGTABLE_DATA_TYPE(arg_date);
+   MRB_ARGTABLE_DATA_TYPE(arg_rem);
+ */
+
+#define MRB_ARGTABLE_DATA_TYPE_OF(argtable_s) mrb_ ## argtable_s ## _data_type
+
 static const struct mrb_data_type mrb_argtable_data_type = {
-  "mrb_argtable_data", mrb_free,
+  "mrb_argtable_data", mrb_argtable_free,
 };
+
+static mrb_value mrb_arg_lit_init(mrb_state *mrb, mrb_value self)
+{
+  arg_lit *data;
+  char *shortopts, *longopts, *glossary;
+
+  data = (arg_lit *)DATA_PTR(self);
+  if (data) {
+    mrb_free(mrb, data);
+  }
+  DATA_TYPE(self) = &MRB_ARGTABLE_DATA_TYPE_OF(arg_lit);
+  DATA_PTR(self) = NULL;
+
+  mrb_get_args(mrb, "zz!z", &shortopts, &longopts, &glossary);
+  data = (arg_lit *)mrb_malloc(mrb, sizeof(arg_lit));
+  data->definition = arg_lit0(shortopts, longopts, glossary);
+  DATA_PTR(self) = data;
+
+  return self;
+}
+
+static mrb_value mrb_arg_lit_count(mrb_state *mrb, mrb_value self)
+{
+  arg_lit *data;
+  data = (arg_lit *)DATA_PTR(self);
+
+  return mrb_fixnum_value(data->definition->count);
+}
 
 static mrb_value mrb_argtable_init(mrb_state *mrb, mrb_value self)
 {
   mrb_argtable_data *data;
-  char *str;
-  int len;
 
   data = (mrb_argtable_data *)DATA_PTR(self);
   if (data) {
@@ -38,21 +91,14 @@ static mrb_value mrb_argtable_init(mrb_state *mrb, mrb_value self)
   DATA_TYPE(self) = &mrb_argtable_data_type;
   DATA_PTR(self) = NULL;
 
-  mrb_get_args(mrb, "s", &str, &len);
   data = (mrb_argtable_data *)mrb_malloc(mrb, sizeof(mrb_argtable_data));
-  data->str = str;
-  data->len = len;
+  data->arg_definitions = mrb_ary_new(mrb);
   DATA_PTR(self) = data;
 
   return self;
 }
 
-static mrb_value mrb_argtable_hello(mrb_state *mrb, mrb_value self)
-{
-  mrb_argtable_data *data = DATA_PTR(self);
-
-  return mrb_str_new(mrb, data->str, data->len);
-}
+/* argtable sample */
 
 static mrb_value mrb_argtable_hi(mrb_state *mrb, mrb_value self)
 {
@@ -93,12 +139,19 @@ static mrb_value mrb_argtable_help(mrb_state *mrb, mrb_value self)
 
 void mrb_mruby_argtable_gem_init(mrb_state *mrb)
 {
-    struct RClass *argtable;
+  struct RClass *argtable,
+    *arg_lit_c /* , *arg_int_c, *arg_dbl_c, *arg_str_c */;
     argtable = mrb_define_class(mrb, "Argtable", mrb->object_class);
-    mrb_define_method(mrb, argtable, "initialize", mrb_argtable_init, MRB_ARGS_REQ(1));
-    mrb_define_method(mrb, argtable, "hello", mrb_argtable_hello, MRB_ARGS_NONE());
+    mrb_define_method(mrb, argtable, "initialize", mrb_argtable_init, MRB_ARGS_NONE());
+
+    /* samples... */
     mrb_define_class_method(mrb, argtable, "sample", mrb_argtable_hi, MRB_ARGS_NONE());
     mrb_define_class_method(mrb, argtable, "help", mrb_argtable_help, MRB_ARGS_NONE());
+
+    arg_lit_c = mrb_define_class_under(mrb, argtable, "Literal", mrb->object_class);
+    mrb_define_method(mrb, arg_lit_c, "initialize", mrb_arg_lit_init,  MRB_ARGS_REQ(3));
+    mrb_define_method(mrb, arg_lit_c, "count",      mrb_arg_lit_count, MRB_ARGS_NONE());
+
     DONE;
 }
 
