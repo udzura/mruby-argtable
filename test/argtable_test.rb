11d3bb60281cd9e69e9ebff5b235164d0a2d0534
Uchio KONDO <udzura@pepabo.com>
2017-06-23 18:41:58 +0900

    Testing hack...

--- test/argtable_test.rb
+++ test/argtable_test.rb
@@ -63,17 +63,3 @@ assert("Argtable#string without optname") do
   assert_equal t.barestring.value, "ryosuke"
   assert_equal t['l'].value, "matsumoto"
 end
-
-assert("Argtable#parse when mixed") do
-  t = Argtable.new
-  t.literal("v", "verbose", "You are verbose")
-  t.integer("c", "count", "<c>", "My count")
-  t.double("s", "seconds", "<t>", "My seconds")
-  t.string("n", "name", "<n>", "My name")
-
-  t.parse(["prog", "-c", "9999", "--verbose", "--name=udzura", "-s", "12.25"])
-  assert_equal t["v"].count, 1
-  assert_equal t["c"].value, 9999
-  assert_equal t["n"].value, "udzura"
-  assert_equal t["s"].value, 12.25
-end
